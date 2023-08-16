c7db3b742ceb455361d5610a7416ee7dc5c48df1
Andre Vargas Roo <a.vargas252@icloud.com>
2023-08-15 22:20:40 -0400

    setup app

--- api/app.py
+++ api/app.py
@@ -2,3 +2,21 @@ import os
 from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Home(Resource):
+
+    def get(self):
+        return 'mahalo'
+
+api.add_resource(Home, '/')
